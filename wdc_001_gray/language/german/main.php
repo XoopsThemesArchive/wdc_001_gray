0ffeac5e1c9389d9208f36a6f3590f79e73ae919
mambax7 <mambax7@gmail.com>
2015-05-05 09:34:25 -0400

    Original files

---
+++ wdc_001_gray/language/german/main.php
@@ -0,0 +1,25 @@
+<?php
+
+// Theme language definitions
+
+// $Id: main.php 2857 2009-03-21 11:12:11Z dhcst $
+// _LANGCODE: de
+// _CHARSET : UTF-8
+// Translator: XOOPS üTranslation Team
+
+/* ------------------- userbar.html ------------------- */
+define("_THEME_REGISTER",           "User Registration");
+define("_THEME_INBOX",              "Mitteilungen anzeigen");
+define("_THEME_INBOX_NOTREAD",      "Nachricht(en) ungelesen");
+define("_THEME_NOTIFICATIONS",      "Benachrichtigungen");
+define("_THEME_PROFILE",            "Profil ansehen");
+define("_THEME_ADMINISTRATION",     "Administration");
+define("_THEME_LOGOUT",             "Abmelden");
+define("_WDC_HELLO",                "Hallo");
+define("_WDC_WELCOME_BACK",         "willkommen zurück bei");
+define("_WDC_REGISTER",             "Registrieren");
+define("_WDC_LOGIN",                "Einloggen");
+define("_WDC_LOGOUT",               "Ausloggen");
+define("_WDC_ACCOUNT",              "Konto");
+define("_WDC_TESTED",               "Läuft unter und ist in getestet in:");
+?>
